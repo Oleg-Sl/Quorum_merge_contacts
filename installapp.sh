c89ecda1eab385c553e8e073dcdd84ee737dd555
Oleg <slepcov_oleg@mail.ru>
2022-10-12 00:07:44 +0700

    Update installapp

--- installapp.sh
+++ installapp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Склонировать репозиторий командой (ввести yes при запросе во время установки):
-git clone https://github.com/Oleg-Sl/Quorum_merge_contacts.git
+#git clone https://github.com/Oleg-Sl/Quorum_merge_contacts.git
 
 # Перейти в директорию загруженного проекта:
-cd QQuorum_merge_contacts/
+#cd QQuorum_merge_contacts/
 
 # Создание docker-образа:
 docker build ./merge_contacts/
